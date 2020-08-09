f85f41ebcd2eb6af2c88c142c086f98503dc7ef1
Administrator <190598743@qq.com>
2020-08-09 20:34:07 +0800

    登陆页面

---
+++ src/main/java/com/example/springbootdemo/controller/Login.java
@@ -0,0 +1,22 @@
+package com.example.springbootdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class Login {
+    @RequestMapping(value = {"","/login1"})
+    public String login1(Model model){
+        model.addAttribute("欢迎登陆","欢迎登陆！");
+        return  "login";
+    }
+
+
+    @RequestMapping(value="/login2", method = RequestMethod.GET)
+    public String login2(Model model){
+        model.addAttribute("欢迎登陆","欢迎登陆！");
+        return  "login2";
+    }
+}
